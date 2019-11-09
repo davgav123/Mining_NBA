24845a9ab99c92be038e20b1d4a1dd4a70326817
davgav123 <davidgavrilovic@hotmail.com>
2019-11-09 01:30:29 +0100

    Changes in src/shooting_analysis.py file
    Additions:
    1. Function 'percentage_of_mid_range_shots()' that calculates percent of all shots that are mid-range shots by team in 2018-19 season. Returns pandas.DataFrame with columns (team name, pct of mid-range) sorted by team name.
    
    2. Function 'compare_ortg_and_mid_range_pct()' that compares ORtg with frequency of mid-range shots by team in 2018-19 season. Function plots scatter plot with values.
    
    3. Function 'print_correlations()' that print correlations between ORtg, TS%, % of mid-range and % of 3-pointers, based on the data from 2000-01 to 2018-19 seasons.
    
    Everyting is needed for documentation.

--- src/shooting_analysis.py
+++ src/shooting_analysis.py
@@ -4,6 +4,8 @@
 import pandas as pd
 from pathlib import Path
 from nba_api.stats.endpoints.shotchartdetail import ShotChartDetail
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def scrap_shooting_data(ids, path_to_dest, from_season='20181001', to_season='20191001'):
@@ -48,6 +50,7 @@ def shot_percentage_by_zone_range_2018_19(print_result=True):
     # attempted shots
     df_attempted = df.groupby('SHOT_ZONE_RANGE').count().reset_index()
 
+    # calculate percentage by zone and add free throw percentage
     shoot_pct_by_zone = {
         '16-24 ft.': shots_by_area(df_made, '16-24 ft.') / shots_by_area(df_attempted, '16-24 ft.'),
         '24+ ft.': shots_by_area(df_made, '24+ ft.') / shots_by_area(df_attempted, '24+ ft.'),
@@ -64,8 +67,91 @@ def shot_percentage_by_zone_range_2018_19(print_result=True):
     return shoot_pct_by_zone
 
 
+def percentage_of_mid_range_shots():
+    df = pd.read_csv(Path('../data/shooting_2018_19_data.csv'))
+
+    df = df[['TEAM_NAME', 'SHOT_ZONE_BASIC']]
+
+    # take mid-range shots and count them by team
+    df_mid_range = df[df['SHOT_ZONE_BASIC'] == 'Mid-Range']
+    df_mid_range_by_team = df_mid_range.groupby('TEAM_NAME').count().reset_index()
+
+    # every shot (including mid-range) count by team
+    df_shots_by_team = df.groupby('TEAM_NAME').count().reset_index()
+
+    # create new dataframe with information about percent of the shots from mid-range for every team
+    # that dataframe will have two columns [team_name, pct_of_mid_range], and it is used to sort by team name
+    df_pct_mid_range = pd.DataFrame(columns=['TEAM_NAME', 'PCT_FROM_MID_RANGE'])
+
+    for team in df_mid_range_by_team['TEAM_NAME'].values:
+        # maybe looks ugly but otherwise the line would be to long
+        pct = float(df_mid_range_by_team[df_mid_range_by_team['TEAM_NAME'] == team]['SHOT_ZONE_BASIC'].values[0]) \
+            / float(df_shots_by_team[df_shots_by_team['TEAM_NAME'] == team]['SHOT_ZONE_BASIC'].values[0])
+
+        # add pair (team, and pct of mid-range for that team) into dataframe
+        df_pct_mid_range = df_pct_mid_range.append(pd.DataFrame([[team, pct]], columns=['TEAM_NAME', 'PCT_FROM_MID_RANGE']))
+
+    # Atlanta first, Washington last
+    df_pct_mid_range = df_pct_mid_range.sort_values(by=['TEAM_NAME'])
+
+    return df_pct_mid_range
+
+
+def compare_ortg_and_mid_range_pct():
+    df_ortg = pd.read_csv(Path('../data/teams_season_2018_19_per_100.csv'))[['Team', 'PTS']]
+    df_pct_mid_range = percentage_of_mid_range_shots()
+
+    # this are the columns of need
+    pct = df_pct_mid_range['PCT_FROM_MID_RANGE'].values
+    ortg = df_ortg['PTS'].values
+
+    plt.scatter(x=pct, y=ortg, s=80)
+
+    # add team name for every dot
+    for index, row in df_ortg.iterrows():
+        plt.text(x=pct[index], y=row['PTS'] + 0.1, s=row['Team'], fontsize=16)
+
+    # add line that represents average ORtg
+    avg = plt.axhline(y=110.4, color='red')
+
+    plt.grid()
+    plt.legend(handles=[avg], labels=['Average ORtg, 110.4'], fontsize=22, loc=4)
+
+    plt.xticks(
+        ticks=np.arange(0.0, 0.31, 0.05),
+        labels=[str(i) + '%' for i in range(0, 31, 5)],
+        fontsize=14
+    )
+    plt.yticks(fontsize=14)
+
+    plt.xlabel('% of shots that were mid-range shots', fontsize=24)
+    plt.ylabel('ORtg', fontsize=24)
+    plt.show()
+
+
+def print_correlations():
+    '''
+    this function prints correlations
+    between ORtg, TS%, %of mid-range attempts
+    and % of 3-point attempts from data
+    from 2000-01 to 2018-19 seasons
+    '''
+
+    df = pd.read_csv(Path('../data/ts_ortg_by_season_data.csv'))
+
+    print('Correlation between TS% and ORtg: {}\n'.format(df['ORtg'].corr(df['TS%'])))
+    print('Correlation between TS% and % of 3PA: {}\n'.format(df['TS%'].corr(df['PCT_3PA'])))
+    print('Correlation between TS% and % of MidRangeAttempts: {}\n'.format(df['TS%'].corr(df['PCT_MidRangeA'])))
+    print('Correlation between % of 3PA and ORtg: {}\n'.format(df['PCT_3PA'].corr(df['ORtg'])))
+    print('Correlation between % of MidRangeAttempts and ORtg: {}\n'.format(df['PCT_MidRangeA'].corr(df['ORtg'])))
+
+
 if __name__ == "__main__":
-    team_ids = list(range(1610612737, 1610612766))
-    scrap_shooting_data(team_ids, '../data/shooting_2018_19_data.csv')
+    team_ids = list(range(1610612737, 1610612767))
+    # scrap_shooting_data(team_ids, '../data/shooting_2018_19_data.csv')
 
     shot_percentage_by_zone_range_2018_19()
+
+    compare_ortg_and_mid_range_pct()
+
+    print_correlations()
